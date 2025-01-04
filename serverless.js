f55094d5993ac0a4f22165ae938b2bfb9e510516
good-mistake <devillmk88@gmail.com>
2025-01-04 21:17:27 +0330

    update routes

--- serverless.js
+++ serverless.js
@@ -19,39 +19,45 @@ module.exports = {
     auth: {
       handler: "auth.handler",
       events: [
-        { http: { path: "/api/auth/login", method: "post" } },
-        { http: { path: "/api/auth/signup", method: "post" } },
-        { http: { path: "/api/auth/verify", method: "post" } },
+        { http: { path: "/api/auth/login", method: "post", cors: true } },
+        { http: { path: "/api/auth/signup", method: "post", cors: true } },
+        { http: { path: "/api/auth/verify", method: "post", cors: true } },
       ],
     },
     budgets: {
       handler: "budgets.handler",
       events: [
-        -{ http: { path: "/api/budgets", method: "get" } } -
-          { http: { path: "/api/budgets/{id}", method: "get" } } -
-          { http: { path: "/api/budgets", method: "post" } } -
-          { http: { path: "/api/budgets/{id}", method: "put" } } -
-          { http: { path: "/api/budgets/{id}", method: "delete" } },
+        { http: { path: "/api/budgets", method: "get", cors: true } },
+        { http: { path: "/api/budgets/{id}", method: "get", cors: true } },
+        { http: { path: "/api/budgets", method: "post", cors: true } },
+        { http: { path: "/api/budgets/{id}", method: "put", cors: true } },
+        { http: { path: "/api/budgets/{id}", method: "delete", cors: true } },
       ],
     },
     transactions: {
       handler: "transactions.handler",
       events: [
-        -{ http: { path: "/api/transactions", method: "get" } } -
-          { http: { path: "/api/transactions/{id}", method: "get" } } -
-          { http: { path: "/api/transactions", method: "post" } } -
-          { http: { path: "/api/transactions/{id}", method: "put" } } -
-          { http: { path: "/api/transactions/{id}", method: "delete" } },
+        { http: { path: "/api/transactions", method: "get", cors: true } },
+        { http: { path: "/api/transactions/{id}", method: "get", cors: true } },
+        { http: { path: "/api/transactions", method: "post", cors: true } },
+        { http: { path: "/api/transactions/{id}", method: "put", cors: true } },
+        {
+          http: {
+            path: "/api/transactions/{id}",
+            method: "delete",
+            cors: true,
+          },
+        },
       ],
     },
     pots: {
       handler: "pots.handler",
       events: [
-        -{ http: { path: "/api/pots", method: "get" } } -
-          { http: { path: "/api/pots/{id}", method: "get" } } -
-          { http: { path: "/api/pots", method: "post" } } -
-          { http: { path: "/api/pots/{id}", method: "put" } } -
-          { http: { path: "/api/pots/{id}", method: "delete" } },
+        { http: { path: "/api/pots", method: "get", cors: true } },
+        { http: { path: "/api/pots/{id}", method: "get", cors: true } },
+        { http: { path: "/api/pots", method: "post", cors: true } },
+        { http: { path: "/api/pots/{id}", method: "put", cors: true } },
+        { http: { path: "/api/pots/{id}", method: "delete", cors: true } },
       ],
     },
   },
