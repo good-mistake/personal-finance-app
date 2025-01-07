a8a1351a289bef1419ce8ef49369882d27f15bf5
good-mistake <devillmk88@gmail.com>
2025-01-08 02:16:17 +0330

    added routes

--- src/components/pages/home/homeService/HomeService.ts
+++ src/components/pages/home/homeService/HomeService.ts
@@ -1,21 +1,26 @@
 import axios from "axios";
 
+const axiosInstance = axios.create({
+  baseURL:
+    "https://personal-finance-app-git-main-goodmistakes-projects.vercel.app",
+});
+
 export const fetchBudgets = async (token: string) => {
-  const response = await axios.get("/api/budgets", {
+  const response = await axiosInstance.get("/api/budgets", {
     headers: { Authorization: `Bearer ${token}` },
   });
   return response.data;
 };
 
 export const fetchTransactions = async (token: string) => {
-  const response = await axios.get("/api/transactions", {
+  const response = await axiosInstance.get("/api/transactions", {
     headers: { Authorization: `Bearer ${token}` },
   });
   return response.data;
 };
 
 export const fetchPots = async (token: string) => {
-  const response = await axios.get("/api/pots", {
+  const response = await axiosInstance.get("/api/pots", {
     headers: { Authorization: `Bearer ${token}` },
   });
   return response.data;
