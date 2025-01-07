03c18f1564316fdafbca80b5a5e795ca8e7a3456
good-mistake <devillmk88@gmail.com>
2025-01-07 21:58:19 +0330

    fixed user routes

--- api/auth/login/index.js
+++ api/auth/login/index.js
@@ -1,4 +1,4 @@
-import User from "../models/models.js";
+import User from "../../../models/models.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import mongoose from "mongoose";
