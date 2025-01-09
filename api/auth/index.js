f2ad98a616dd3bc147f94bfa1a3e0f54f1f8eac3
good-mistake <devillmk88@gmail.com>
2025-01-09 19:04:03 +0330

    added other methods to header

--- api/auth/index.js
+++ api/auth/index.js
@@ -18,8 +18,14 @@ export default async function handler(req, res) {
     res.setHeader("Access-Control-Allow-Origin", "*");
   }
 
-  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "POST, OPTIONS, GET, PUT, DELETE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, X-Requested-With"
+  );
 
   if (req.method === "OPTIONS") {
     return res.status(200).end();
--- src/components/pages/home/homeService/HomeService.ts
+++ src/components/pages/home/homeService/HomeService.ts
@@ -6,21 +6,21 @@ const axiosInstance = axios.create({
 });
 
 export const fetchBudgets = async (token: string) => {
-  const response = await axiosInstance.get("/api/auth/budgets", {
+  const response = await axiosInstance.get("/api/budgets", {
     headers: { Authorization: `Bearer ${token}` },
   });
   return response.data;
 };
 
 export const fetchTransactions = async (token: string) => {
-  const response = await axiosInstance.get("/api/auth/transactions", {
+  const response = await axiosInstance.get("/api/transactions", {
     headers: { Authorization: `Bearer ${token}` },
   });
   return response.data;
 };
 
 export const fetchPots = async (token: string) => {
-  const response = await axiosInstance.get("/api/auth/pots", {
+  const response = await axiosInstance.get("/api/pots", {
     headers: { Authorization: `Bearer ${token}` },
   });
   return response.data;
