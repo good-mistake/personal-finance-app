cd751ed84d30867870b76a7c2d2ed3ae5f1d763e
good-mistake <devillmk88@gmail.com>
2025-01-13 17:08:00 +0330

    changed it for 405 err

--- api/pots/index.js
+++ api/pots/index.js
@@ -17,10 +17,7 @@ export default async function handler(req, res) {
     res.setHeader("Access-Control-Allow-Origin", "*");
   }
 
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, OPTIONS"
-  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
   res.setHeader(
     "Access-Control-Allow-Headers",
     "Content-Type, Authorization, X-Requested-With"
@@ -53,48 +50,7 @@ export default async function handler(req, res) {
       return res.status(201).json(newPot);
     }
 
-    if (method === "PUT") {
-      const { id, ...updateFields } = req.query; // Assuming you're passing the pot ID in the query
-
-      if (!id) {
-        return res
-          .status(400)
-          .json({ message: "Pot ID and amount are required" });
-      }
-
-      const pot = await Pot.findById(id);
-
-      if (!pot) {
-        return res.status(404).json({ message: "Pot not found" });
-      }
-
-      const updatedpots = await Pot.findByIdAndUpdate(id, updateFields, {
-        new: true,
-        runValidators: true,
-      });
-      if (!updatedpots) {
-        return res.status(404).json({ message: "Pot not found" });
-      }
-      return res.status(200).json(pot);
-    }
-
-    if (method === "DELETE") {
-      const { id } = req.query;
-
-      if (!id) {
-        return res.status(400).json({ message: "Pot ID is required" });
-      }
-
-      const pot = await Pot.findByIdAndDelete(id);
-
-      if (!pot) {
-        return res.status(404).json({ message: "Pot not found" });
-      }
-
-      return res.status(200).json({ message: "Pot deleted successfully" });
-    }
-
-    res.setHeader("Allow", ["GET", "POST", "PUT", "DELETE"]);
+    res.setHeader("Allow", ["GET", "POST"]);
     return res.status(405).json({ message: `Method ${method} Not Allowed` });
   } catch (error) {
     console.error("Server error:", error);
