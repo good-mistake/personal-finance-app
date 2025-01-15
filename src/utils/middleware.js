ca8b4b3ae6ccb54cb3029740bf051b61b271b883
good-mistake <devillmk88@gmail.com>
2025-01-15 22:05:37 +0330

    changed handling token

--- src/utils/middleware.js
+++ src/utils/middleware.js
@@ -2,15 +2,15 @@ import jwt from "jsonwebtoken";
 import User from "../../models/models.js";
 
 export const authenticateToken = async (req, res, next) => {
-  let token = req.headers.authorization?.split(" ")[1];
-  console.log(token);
+  let token = req.headers.authorization?.split(" ")[1]; // Extract token from the header
+  console.log("Token in authenticate:", token);
 
   if (!token) {
     if (req.user?.id) {
       try {
         const user = await User.findById(req.user.id);
         if (user && user.refreshToken) {
-          token = user.refreshToken;
+          token = user.refreshToken; // Use refreshToken if the access token is not provided
         }
       } catch (error) {
         console.error("Error fetching user:", error);
@@ -26,6 +26,7 @@ export const authenticateToken = async (req, res, next) => {
   }
 
   try {
+    // Here we use the same process as in login to verify the token
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = { id: decoded.id };
     next();
