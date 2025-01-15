630ce256c68ceaf664057fdab8dbaf53d967462e
good-mistake <devillmk88@gmail.com>
2025-01-15 16:28:26 +0330

    console log added for err

--- src/utils/middleware.js
+++ src/utils/middleware.js
@@ -2,6 +2,7 @@ import jwt from "jsonwebtoken";
 
 export const authenticateToken = (req, res, next) => {
   const token = req.headers.authorization?.split(" ")[1];
+  console.log("Received token:", token);
   if (!token) {
     return res
       .status(401)
@@ -10,9 +11,11 @@ export const authenticateToken = (req, res, next) => {
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    console.log("Decoded token:", decoded);
     req.user = { id: decoded.id };
     next();
   } catch (error) {
+    console.error("Token verification error:", error);
     if (error.name === "TokenExpiredError") {
       return res
         .status(403)
